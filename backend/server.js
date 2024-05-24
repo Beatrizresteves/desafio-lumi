27ee1ed298556567b89927daacaec7ebad701c37
Beatrizresteves <beatrizramalho.esteves@gmail.com>
2024-05-24 12:12:02 -0300

    Add novas configs

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,10 @@
-// server.js
 const express = require('express');
-const { Pool } = require('pg');
+const db = require('./models');
+
 const app = express();
-const port = 3000;
 
-const pool = new Pool({
-  user: 'your_dbuser',
-  host: 'your_dbhost',
-  database: 'your_dbname',
-  password: 'your_dbpassword',
-  port: 'your_dbport',
-});
-
-app.use(express.json());
-
-app.get('/faturas', async (req, res) => {
-  try {
-    const result = await pool.query('SELECT * FROM faturas');
-    res.json(result.rows);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Server error');
-  }
-});
-
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+db.sequelize.sync().then(() => {
+  app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+  });
 });
