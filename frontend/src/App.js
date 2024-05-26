16b08ecb14bf6626c4b8533b04aaf2734626142a
Beatrizresteves <Beatrizresteves@github.com>
2024-05-26 20:30:39 -0300

    Coloca em pagina principal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,36 @@
+// App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import BibliotecaFaturas from './components/BibliotecaFaturas';
+import './App.css';
+
+const HomePage = () => {
+  return (
+    <div className="home-page">
+      <h1>Bem-vindo!</h1>
+      <p>Escolha uma opção:</p>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/dashboard" className="nav-link">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/biblioteca-faturas" className="nav-link">Biblioteca de Faturas</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
 const App = () => {
   return (
     <Router>
       <div>
         <Routes>
-          <Route path="/" element={<Dashboard />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/biblioteca-faturas" element={<BibliotecaFaturas />} />
         </Routes>
       </div>
